e9b81e6260802181e87019454fbf248e8858a926
Thammakit <jthammakit2546@gmail.com>
2025-06-18 15:31:31 +0700

    lesson next route

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Card, Carousel, Input } from 'antd';
+import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+
+import { CiSearch } from 'react-icons/ci';
+import Image from 'next/image';
+import BurgerPromo from '@/assets/icons/burger-icon.svg';
+import CategloryPizza from '@/assets/icons/categlory-pizza.svg';
+import CategloryHambur from '@/assets/icons/categlory-hamburger.svg';
+import CateglorySanwish from '@/assets/icons/categlory-sandwish.svg';
+
+import Banner1 from '@/assets/icons/banner-promotion.jpg';
+import Banner2 from '@/assets/icons/banner-promotion2.jpg';
+
+import Meta from 'antd/es/card/Meta';
+
+export default function MenuPage() {
+  
+  return (
+<div className=''>
+  this is login page
+</div>
+  );
+}
