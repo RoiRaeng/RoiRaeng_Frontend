9788b8c3a1531154eed06685fc133ad040eec515
Nisreen46 <nisreen5258503@gmail.com>
2025-06-20 10:59:39 +0700

    refactor code for login page

---
+++ frontend/src/app/login/[forgot-password]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <div>
+      <h1>ลืมรหัสผ่าน</h1>
+    </div>
+  )
+}
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import { Input } from 'antd';
-// import Icon from './public/icon/globe.svg'
-// D:\Roi Raeng\Frontend\frontend\public\globe.svg
+import React from "react";
+import { Input, Checkbox, Button } from "antd";
+import UserIcon from "@/assets/icons/user-icon.svg";
+import LockIcon from "@/assets/icons/lock-icon.svg";
+import Burger from "@/assets/icons/burgur-1.jpg";
+import Image from "next/image";
 
-function LoginPage() {
+export default function LoginPage() {
   return (
-    <div className='min-h-screen w-full bg-[#DB3B5E]'>
-      <h3 className='text-2xl text-white font-bold'>เข้าสู่ระบบ</h3>
-      <Input 
-        size='large' 
-        // addonAfter={Icon}
-        placeholder='ชื่อผู้ใช้' 
-        className='bg-red-500'>
-      </Input>
-    </div>
-  )
-}
+    <div>
+      <div className="flex min-h-screen w-full bg-[#DB3B5E]">
+        <div className="w-1/2 p-4 sm:p-8 md:p-12 lg:p-16 xl:p-40 flex flex-col justify-center items-center gap-6">
+          <h3 className="text-3xl text-white font-bold pb-4">เข้าสู่ระบบ</h3>
+          <Input
+            size="large"
+            placeholder="ชื่อผู้ใช้"
+            className="!bg-white/10 !rounded-[16px] !h-[50px] !text-white"
+            suffix={<Image src={UserIcon} alt="user-icon" height={20} />}
+          ></Input>
 
-export default LoginPage
+          <Input
+            size="large"
+            placeholder="รหัสผ่าน"
+            className="!bg-white/10 !rounded-[16px] !h-[50px] !text-white"
+            suffix={<Image src={LockIcon} alt="user-icon" height={20} />}
+          ></Input>
+
+          <div className="flex justify-between w-full">
+            <Checkbox className="text-sm !text-white">จดจำผู้ใช้</Checkbox>
+            <a href="/login/forgot-password" className="text-sm text-white">
+              ลืมรหัสผ่าน
+            </a>
+          </div>
+
+          <button className="w-full bg-white text-black font-bold py-2 rounded-xl h-[50px]">
+            <a href="/navbar">เข้าสู่ระบบ</a>
+            
+          </button>
+        </div>
+
+        <div className="w-1/2">
+          <Image src={Burger} alt="user-icon" className="w-full h-full rounded-tl-[50px]" />
+        </div>
+      </div>
+    </div>
+  );
+}
