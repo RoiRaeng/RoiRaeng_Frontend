1e3f8d1b95567d57cadb87c6ec298e7165959746
Thammakit <jthammakit2546@gmail.com>
2025-06-23 13:38:00 +0700

    add new path

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/login',
+        destination: '/home?table=NTpidXJnZXItc2VjcmV0LWtleQ==',
         permanent: false,
       },
     ];
